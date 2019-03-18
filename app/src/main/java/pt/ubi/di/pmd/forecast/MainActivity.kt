6b29e0b2f99f478ef3e7b63d217545d970df4bf2
Jakub Kucharz <ruseknobusek@wp.pl>
2019-03-18 17:51:59 +0100

    Initial commit

---
+++ app/src/main/java/pt/ubi/di/pmd/forecast/MainActivity.kt
@@ -0,0 +1,12 @@
+package pt.ubi.di.pmd.forecast
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
